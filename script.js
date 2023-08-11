ca4405af81c24230cd87e4a0debd9be85f21b15a
yvzrob1219 <robbaban12@gmail.com>
2023-08-11 22:39:46 +0800

    3rd commit

--- script.js
+++ script.js
@@ -1,64 +1,55 @@
-// first i use unsplash API
+// I use unsplash API
 const accessKey = "XXqChb_B3WhNxucKshCqC7_C-Oj4LRILVBzwPiH14nA";
 
-// then we Select various elements from the HTML document
-const formEl = document.querySelector("form"); 
-const inputEl = document.getElementById("search-input"); t
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
 const searchResults = document.querySelector(".search-results");
-const showMore = document.getElementById("show-more-button"); 
+const showMore = document.getElementById("show-more-button");
 
-//and Initialize variables for tracking 
-let inputData = ""; 
-let page = 1; 
+let inputData = "";
+let page = 1;
 
-// perform image search using Unsplash API
 async function searchImages() {
     inputData = inputEl.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-    // Fetch data from the API
     const response = await fetch(url);
     const data = await response.json();
 
-    const results = data.results; 
+    const results = data.results;
 
     if (page === 1) {
-        searchResults.innerHTML = ""; 
+        searchResults.innerHTML = "";
     }
 
-    // Loop through the search results and create elements to display images and links
     results.map((result) => {
         const imageWrapper = document.createElement("div");
-        imageWrapper.classList.add("search-result"); // Apply a CSS class to the image wrapper
+        imageWrapper.classList.add("search-result");
         const image = document.createElement("img");
-        image.src = result.urls.small; 
-        image.alt = result.alt_description; // Set the alt text for the image
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
         const imageLink = document.createElement("a");
-        imageLink.href = result.links.html; // Set the link URL to the Unsplash page of the image
-        imageLink.target = "_blank"; 
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
         imageLink.textContent = result.alt_description;
 
         imageWrapper.appendChild(image);
         imageWrapper.appendChild(imageLink);
-        searchResults.appendChild(imageWrapper); // Add the image wrapper to the search results container
+        searchResults.appendChild(imageWrapper);
     });
 
-    page++; // Increment the page number for the next search
-
-    // Display the "Show More" button if there are more than one page of results
+    page++;
     if (page > 1) {
         showMore.style.display = "block";
     }
 }
 
-
 formEl.addEventListener("submit", (event) => {
-    event.preventDefault(); 
-    page = 1; 
-    searchImages(); 
+    event.preventDefault();
+    page = 1;
+    searchImages();
 });
 
-// Event listener for "Show More" button click
 showMore.addEventListener("click", () => {
-    searchImages(); 
+    searchImages();
 });
